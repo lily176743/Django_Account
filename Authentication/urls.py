02ad9f9ad0e942d4d9afc3a76b20e61247b334b0
StevenHocking <milkyway464203@gmail.com>
2023-06-11 17:00:25 +0900

    KSG changed

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #KSG comment added
     path('signin/', views.signin, name="signin"),
     path('signup/', views.signup, name="signup"),
     path('signout/', views.signout, name="signout"),
