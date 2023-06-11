4b97898df27e47397cb562b4a3d96e2ef689dc38
StevenHocking <milkyway464203@gmail.com>
2023-06-11 17:17:45 +0900

    KSG newly changed

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     #KSG comment added
+    #KSG comment added -2
     path('signin/', views.signin, name="signin"),
     path('signup/', views.signup, name="signup"),
     path('signout/', views.signout, name="signout"),
